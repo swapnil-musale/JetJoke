a4729db59e13b6e79793c11ff7595b49bcfbabc4
Swapnil Musale <swapnilmusale19@gmail.com>
2023-08-28 13:43:47 +0530

    Bump VersionName and VersionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.devx.jetjoke"
         minSdk = 21
         targetSdk = 34
-        versionCode = 31
-        versionName = "1.0.4"
+        versionCode = 32
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
